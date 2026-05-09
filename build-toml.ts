1b69caa487202acee9240ec55b7fff6a1aae2342
tinarskii <tinvv@outlook.co.th>
2026-05-09 20:45:38 +0700

    chore: rebuild existing .pw.toml files

--- build-toml.ts
+++ build-toml.ts
@@ -1,39 +1,41 @@
 // @ts-nocheck using Bun
 async function getFiles(root: string) {
-    const files: string[] = [];
+	const files: string[] = [];
 
-    async function walk(dir: string) {
-        for await (const entry of new Bun.Glob("**/*").scan({
-            cwd: dir,
-            absolute: true,
-            onlyFiles: true,
-        })) {
-            files.push(entry);
-        }
-    }
+	async function walk(dir: string) {
+		for await (const entry of new Bun.Glob("**/*").scan({
+			cwd: dir,
+			absolute: true,
+			onlyFiles: true,
+		})) {
+			files.push(entry);
+		}
+	}
 
-    await walk(root);
+	await walk(root);
 
-    return files
+	return files
 }
 
-const files = await getFiles("./public/config");
+const config_files = await getFiles("./public/config");
+const scripts_files = await getFiles("./public/scripts");
+const resourcepacks_files = await getFiles("./public/resourcepacks");
 
 async function sha256File(path) {
-    const file = Bun.file(path);
-    const buffer = await file.arrayBuffer();
+	const file = Bun.file(path);
+	const buffer = await file.arrayBuffer();
 
-    return new Bun.CryptoHasher("sha256")
-    .update(buffer)
-    .digest("hex");
+	return new Bun.CryptoHasher("sha256")
+	.update(buffer)
+	.digest("hex");
 }
 
-for (const file of files) {
-    let relPath = file.split("/").slice(6).join("/");
-    let urlEncodedRelPath = relPath.split("/").map(encodeURIComponent).join("/");
-    let hash = await sha256File(file);
-    let content =
-`name = "${file.split("/").slice(-1)[0]}"
+async function createContent(file: string) {
+	let relPath = file.split("/").slice(6).join("/");
+	let urlEncodedRelPath = relPath.split("/").map(encodeURIComponent).join("/");
+	let hash = await sha256File(file);
+	let content =
+		`name = "${file.split("/").slice(-1)[0]}"
 filename = "${file.split("/").slice(-1)[0]}"
 side = "both"
 
@@ -44,5 +46,17 @@ url = "https://scentcraft-modpack.netlify.app/public/${urlEncodedRelPath}"
 # packwiz supports a number of hashes, including sha256, sha512, sha1 and md5
 hash-format = "sha256"
 hash = "${hash}"`
-    Bun.write(`./${relPath}.pw.toml`, content);
+	Bun.write(`./${relPath}.pw.toml`, content);
+}
+
+for (const file of config_files) {
+	await createContent(file);
+}
+
+for (const file of scripts_files) {
+	await createContent(file);
+}
+
+for (const file of resourcepacks_files) {
+	await createContent(file);
 }
